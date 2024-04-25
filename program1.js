ca0d8efe124eb6a5c3be5e751c27e27ab34e889a
Ritu0425 <168063542+Ritu0425@users.noreply.github.com>
2024-04-25 01:41:20 -0700

    Add files via upload

--- program1.js
+++ program1.js
@@ -1,7 +1,33 @@
-function longestSubstring(s) {
-    // Implementation of longestSubstring function
-}
+
+    function longestsubstring(s)
+
+
+
+
+{
+    //  Implementation of longestSubstring function
+    let  longestStringlength=0;
+
+    for (let i=0; i<s.length; i++)
+    {
+
+        let currentString =new Set();
+        for (let x=i; x<s.length;i++)
+        {
+            if currentString.has(s[x])
+            {
+                break;
+            }
+            else{
+                currentString.add(s[x]);
+            }
+        }
+        longestStringlength=Math.max(
+            longestStringlength,currentString.size
+        );
+    }
+
+    return longestStringlength;
+};
 
 module.exports = { longestSubstring };
-
-
